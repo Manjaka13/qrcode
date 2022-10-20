80c3330418b6ef90980a61f0e3a5717fb129fe28
harijaona <harijaona@expat.com>
2022-10-20 20:36:47 +0300

    setup main component

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,9 +1,38 @@
 <template>
-	<h1>Hello world !</h1>
+	<div class="container">
+		<h1 class="container__title">QR Code generator</h1>
+		<hr />
+		<input
+			class="container__input"
+			placeholder="Enter string"
+			type="text"
+			v-model="url"
+		/>
+		<div class="container__qr">
+			<vue-qrious :value="url" mime="image/webp" :size="350" />
+		</div>
+		<p class="container__copyright">
+			Made with <span class="red">♥</span> by
+			<a class="link" href="https://github.com/Manjaka13" title="Author"
+				>Hari</a
+			>
+		</p>
+	</div>
 </template>
 
 <script>
+import VueQrious from "vue-qrious";
+
 export default {
 	name: "App",
+	components: {
+		VueQrious,
+	},
+	data() {
+		return {
+			url: "Hari",
+		};
+	},
+	methods: {},
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import App from "@/components/App.vue";
+import "@/styles/index.scss";
 
 /**
  * Entry point
